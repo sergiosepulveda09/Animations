ba35a93f5a0aabd4f92f55996efb3de8f38ea883
Sergio <sergiosepulveda09@live.com>
2021-06-30 13:47:47 -0700

    Initial Commit

---
+++ AnimationsProject/AnimationsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsProjectApp.swift
+//  AnimationsProject
+//
+//  Created by Sergio Sepulveda on 2021-06-30.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
